d5d213eb37e9d9cf83ed293eeb1dea6b2d500158
Azeyn <hi@azeyn.com>
2021-03-06 10:53:40 +0000

    Prepare for NPM module

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@
  * The galleries plugin is licensed under the PolyForm Noncommercial License 1.0.0
  */
 
+import { createApp } from 'vue'
 import Gallery from './components/Gallery'
 
-export default Gallery
+const app = createApp({})
+app.component('gallery', Gallery)
+app.mount('#gallery-container')
